65d842e7b60cf6bcd1e04c285920f2c7d7ea6ac2
Simon <75579810+sbrrr@users.noreply.github.com>
2020-12-07 00:37:34 +0100

    List exercises

--- exercise302.py
+++ exercise302.py
@@ -1,1 +1,1 @@
-a = [1, 1, 3, 3, 5, 8, 13, 21, 34, 55, 89]\n b=[]\n for element in a:\n     if element<=5:\n        b.append(element)\n print(b)
+print([i for i in [1, 2, 3 ,4 ,5 , 8, 13, 21, 34, 55, 89] if i <= 5])
---
+++ exercise3033.py
@@ -0,0 +1,3 @@
+c=int(input("Enter number: "))
+a = [1, 1, 3, 3, 5, 8, 13, 21, 34, 55, 89]
+print([i for i in a if i <= c])
