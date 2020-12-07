a92df9284728e9d006327a0df76e5e0465824c0b
Simon <75579810+sbrrr@users.noreply.github.com>
2020-12-07 02:55:03 +0100

    More exercises

---
+++ exercise4.py
@@ -0,0 +1,2 @@
+num = int(input("Enter number: "))
+print([i for i in range(1, num) if num % i == 0])
---
+++ exercise5_1.py
@@ -0,0 +1,3 @@
+a = [13, 20, 8, 18, 10]
+b = [15, 13, 4, 11, 12, 18]
+print(list(set(i for i in a if i in b)))
